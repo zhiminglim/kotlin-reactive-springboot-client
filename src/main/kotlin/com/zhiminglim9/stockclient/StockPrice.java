c3564b3f358bc71291c557868fbee96eb788e57a
Zhiming <i-jovial@hotmail.com>
2022-01-22 16:20:25 +0800

    initial commit

---
+++ src/main/kotlin/com/zhiminglim9/stockclient/StockPrice.java
@@ -0,0 +1,16 @@
+package com.zhiminglim9.stockclient;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StockPrice {
+    private String symbol;
+    private Double price;
+    private LocalDateTime time;
+}
---
+++ src/main/kotlin/com/zhiminglim9/stockclient/WebClientStockClient.java
@@ -0,0 +1,25 @@
+package com.zhiminglim9.stockclient;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+
+import java.io.IOException;
+
+@Log4j2
+public class WebClientStockClient {
+    private WebClient webClient;
+
+    public WebClientStockClient(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public Flux<StockPrice> pricesFor(String symbol) {
+        return webClient.get()
+                .uri("http://localhost:8080/stocks/{symbol}", symbol)
+                .retrieve()
+                .bodyToFlux(StockPrice.class)
+                .retry(5)
+                .doOnError(IOException.class, e -> log.error(e.getMessage()));
+    }
+}
